30b50ac59466baa51832fe220d3c2994c2a09710
InventedMost <netvid00@gmail.com>
2022-07-10 11:35:18 +0100

    added nexus queries for users and task

--- pages/api/tasks.ts
+++ pages/api/tasks.ts
@@ -1,4 +1,4 @@
-import { ApolloServer, ApolloServer } from "apollo-server-micro"
+import { ApolloServer } from "apollo-server-micro"
 import { typeDefs } from "../../graphql/schema"
 import resolvers from "../../graphql/resolvers"
 import Cors from "micro-cors"
